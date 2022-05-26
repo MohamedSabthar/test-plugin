aec8b99aba3a0c06e0dc007dbcacabe6791dd11a
MohamedSabthar <sabthar@wso2.com>
2022-05-26 18:59:02 +0530

    Update BallerinaPlugin.groovy

--- src/main/groovy/io/ballerina/plugin/BallerinaPlugin.groovy
+++ src/main/groovy/io/ballerina/plugin/BallerinaPlugin.groovy
@@ -244,7 +244,9 @@ class BallerinaPlugin implements Plugin<Project> {
                         workingDir project.projectDir
                         environment 'JAVA_OPTS', '-DBALLERINA_DEV_COMPILE_BALLERINA_ORG=true'
                         if (checkForBreakingChanges) {
-                             commandLine 'sh', '-c', "docker run --net=host  --user \$(id -u):\$(id -g) -v $project.projectDir/..:/home -v $project.projectDir:/home/ballerina ballerina/ballerina:nightly $balJavaDebugParam bal pack --target-dir ${balBuildTarget} --offline ${debugParams}"
+                             //commandLine 'sh', '-c', "docker run --net=host  --user \$(id -u):\$(id -g) -v $project.projectDir/..:/home -v $project.projectDir:/home/ballerina ballerina/ballerina:nightly $balJavaDebugParam bal pack --target-dir ${balBuildTarget} --offline ${debugParams}"
+                            commandLine 'sh', '-c', "docker run --net=host  --user \$(id -u):\$(id -g) -v $project.projectDir/..:/home -v $project.projectDir:/home/ballerina ballerina/ballerina:nightly ls"
+                            commandLine 'sh', '-c', "docker run --net=host  --user \$(id -u):\$(id -g) -v $project.projectDir/..:/home -v $project.projectDir:/home/ballerina ballerina/ballerina:nightly echo $(cd ..) & ls"
                         } else if (Os.isFamily(Os.FAMILY_WINDOWS)) {
                             commandLine 'cmd', '/c', "$balJavaDebugParam $distributionBinPath/bal.bat pack --target-dir ${balBuildTarget} --offline ${debugParams} && exit %%ERRORLEVEL%%"
                         } else {
